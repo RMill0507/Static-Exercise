eafca1e2307ada853910697fa26a016484a8186b
Rob <Rmilla445@gmail.com>
2022-10-31 15:31:51 -0400

    Static exercise completed

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            var result = (f - 32) / 1.8;
+            return result;
+        }
+        public static double CelsiusToFahrenheit(double c)
+        {
+            var result = (c * 9) / 5 + 32;
+            return result;
+        }
+
+    
+    
+    
+    
+    
+    
+    }
+}
